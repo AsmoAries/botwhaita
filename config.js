9e9e1ff77c84ab423ae39de96c1ce76a9e766581
AsmoAries <147879076+AsmoAries@users.noreply.github.com>
2023-10-13 22:02:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,15 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
-  ['393518398856', 'nobody|'],
-  ['393755775755', 'nobody|'],
-  ['393755575577', 'nobody|'],
-  ['33760536110', 'nobody|'],
+  ['6432289684', 'asmo|'],
+  ['6432289684', 'asmo|'],
+  ['6432289684', 'asmo|'],
+  ['6432289684', 'asmo|'],
+  ['6432289684', 'asmo|'],
 ]
-global.suittag = ['390371717717'] 
-global.prems = ['390371717717'] 
-global.reportes_solicitudes = ['390371717717']
+global.suittag = ['6432289684'] 
+global.prems = ['6432289684'] 
+global.reportes_solicitudes = ['6432289684']
 
 global.packname = '(☞ﾟ∀ﾟ)☞'
 global.author = '★ცơɬῳɧąıɬą★'
